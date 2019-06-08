0b50d759a4c0dcd4116635a224b440be04c96cdd
Andrew Miller <ajamesmiller35@gmail.com>
2019-06-08 14:45:56 -0600

    Minor content addition. Removed Tip Jar Poll. Fixed Scrolling bug.

--- redeeminggrounds/clickscroll-rg.js
+++ redeeminggrounds/clickscroll-rg.js
@@ -16,17 +16,20 @@
                 $('html, body').animate({
                     scrollTop: ($(target).offset().top - $("#rg-banner").height() - 60)
                   }, 1000);
+                  console.log(" < 1100!!!");
              }
              else if ($(window).scrollTop() >= height && $('body').width() >= 1100){
                 $('html, body').animate({
                     scrollTop: ($(target).offset().top - $("#nav-height").height() - 125)
                   }, 1000);
+                  console.log(" >= 1100!!!");
              }
              else{
                 $('html, body').animate({
-                    scrollTop: ($(target).offset().top - $('#rg-banner').height() + 20)
+                    scrollTop: ($(target).offset().top - $('#rg-banner').height() - 350)
                   }, 1000);
                   console.log('height:' + $('#rg-banner').height());
+                  console.log(" ELSE!! ");
              }
 
           });
