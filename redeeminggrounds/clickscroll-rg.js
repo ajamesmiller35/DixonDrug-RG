c61730c907a7b358df17815b39faba0ca1ecf0d6
Andrew Miller <ajamesmiller35@gmail.com>
2019-06-08 18:47:38 -0600

    updated clickscroll settings

--- redeeminggrounds/clickscroll-rg.js
+++ redeeminggrounds/clickscroll-rg.js
@@ -12,24 +12,25 @@
 
              var height = $("#rg-banner").height() + 15;
 
-             if ($(window).scrollTop() >= height && $('body').width() < 1100){
+             if ($(window).scrollTop() <= height && $('body').width() < 1100){
                 $('html, body').animate({
-                    scrollTop: ($(target).offset().top - $("#rg-banner").height() - 60)
+                    scrollTop: ($(target).offset().top - $("#rg-banner").height() - 350)
                   }, 1000);
-                  console.log(" < 1100!!!");
              }
              else if ($(window).scrollTop() >= height && $('body').width() >= 1100){
                 $('html, body').animate({
                     scrollTop: ($(target).offset().top - $("#nav-height").height() - 125)
                   }, 1000);
-                  console.log(" >= 1100!!!");
              }
+             else if ($(window).scrollTop() < height && $('body').width() >= 1100){
+              $('html, body').animate({
+                  scrollTop: ($(target).offset().top - $("#rg-banner").height() + 25)
+                }, 1000);
+           }
              else{
                 $('html, body').animate({
-                    scrollTop: ($(target).offset().top - $('#rg-banner').height() - 350)
+                    scrollTop: ($(target).offset().top - $('#rg-banner').height() - 60)
                   }, 1000);
-                  console.log('height:' + $('#rg-banner').height());
-                  console.log(" ELSE!! ");
              }
 
           });
